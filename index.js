fe23b63ada91ec663ec517177c797f56a98066fd
amanda ranero <amandaranero@gmail.com>
2023-01-25 17:15:48 -0500

    commit tree 7d689b3a59106d27caa3ec6e083d94596cc44de4

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    let newCat = [...cats, `${name}`];
+    return newCat
+}
+
+function prependCat(name){
+    let newCat = [`${name}`, ...cats];
+    return newCat;
+}
+
+function removeLastCat(){
+    let newCat = cats.slice(0,2);
+    return newCat
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+    
+}
